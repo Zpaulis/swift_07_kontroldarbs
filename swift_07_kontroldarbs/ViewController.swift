c42843e37434f3afa2ff65b86d39563be4388101
Paulis Zabarovskis <paulis@zabarovski.lv>
2022-06-17 23:27:43 +0300

    Initial Commit

---
+++ swift_07_kontroldarbs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_07_kontroldarbs
+//
+//  Created by Paulis Zabarovskis on 17/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
